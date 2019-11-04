ccfed223bf3caa196957816a040f16504d29607e
Udeni dilrukshi <40499515+udenidilru@users.noreply.github.com>
2019-11-04 17:47:00 +0530

    modified

--- crud/src/app/employee/employee-list/employee-list.component.ts
+++ crud/src/app/employee/employee-list/employee-list.component.ts
@@ -2,6 +2,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
 import { Employee } from 'src/app/shared/employee.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-employee-list',
@@ -11,7 +12,8 @@ import { Employee } from 'src/app/shared/employee.model';
 export class EmployeeListComponent implements OnInit {
   list: Employee[];
   constructor(private service: EmployeeService,
-              private firestore: AngularFirestore) { }
+              private firestore: AngularFirestore,
+              private toastr: ToastrService) { }
  
   ngOnInit() {
     this.service.getEmployees().subscribe(actionArray => {
@@ -32,5 +34,6 @@ export class EmployeeListComponent implements OnInit {
     if (confirm('Are you sure to delete this record?')) {
       this.firestore.doc('employees/' + id).delete();
     }
+    this.toastr.warning('Deleted successfully','EMP. Register');
   }
 }
